814033c45ec7b85de72e4e45a0c2badcd56fcb2e
gobattle <wangtaizhou@gmail.com>
2013-06-23 16:54:15 +0800

    :init

---
+++ lib/aliases.zsh
@@ -0,0 +1,25 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+alias please='sudo'
+
+#alias g='grep -in'
+alias pg='ps -aux | grep'
+
+# Show history
+alias history='fc -l 1'
+
+# List direcory contents
+alias l='ls -la'
+alias ll='ls -lahF'
+alias sl=ls # often screw this up
+
+alias afind='ack-grep -il'
+
