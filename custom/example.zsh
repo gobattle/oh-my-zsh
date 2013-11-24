1baa30fbb05542ed084f2ca8efac8c467454dcbd
gobattle <wangtaizhou@gmail.com>
2013-11-24 23:44:55 +0800

    Making SSH_AUTH_SOCK Work Between Detaches in Tmux

--- custom/example.zsh
+++ custom/example.zsh
@@ -2,4 +2,10 @@
 # Example:
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
+
+# Making SSH_AUTH_SOCK Work Between Detaches in Tmux
+if [ ! -z "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ] ; then
+    unlink "$HOME/.ssh/agent_sock" 2>/dev/null
+    ln -s "$SSH_AUTH_SOCK" "$HOME/.ssh/agent_sock"
+    export SSH_AUTH_SOCK="$HOME/.ssh/agent_sock"
+fi
